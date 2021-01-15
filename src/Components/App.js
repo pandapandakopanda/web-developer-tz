2356e4566373811c63d34cc9c3ce78fe1b35aa57
Julia <anything.can.happen.in.the.next.hour@gmail.com>
2021-01-15 14:02:57 +0300

    smth

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
-import { inject } from 'mobx-react'
+import { inject, observer } from 'mobx-react'
 import React from 'react'
 import ST from './index.scss'
 
 import WorkField from './WorkField'
 
 @inject('store')
+@observer
 class App extends React.Component {
 
     componentDidMount(){
